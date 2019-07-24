788924cf71be96bd3048d8bb4830d88f186a6b84
martin.frank <martin.frank.private@gmail.com>
2019-07-24 12:47:12 +0200

    https://github.com/martinFrank/idlelib/projects/1#card-24348096

---
+++ src/main/java/com/github/martinfrank/idlelib/ClickValue.java
@@ -0,0 +1,22 @@
+package com.github.martinfrank.idlelib;
+
+import java.util.concurrent.TimeUnit;
+
+public class ClickValue {
+
+    private final long timeAmount;
+    private final TimeUnit timeUnit;
+
+    public ClickValue(int value, TimeUnit timeUnit) {
+        this.timeAmount = value;
+        this.timeUnit = timeUnit;
+    }
+
+    public long getTimeAmount() {
+        return timeAmount;
+    }
+
+    public TimeUnit getTimeUnit() {
+        return timeUnit;
+    }
+}
