3e57533ba7d18f75fc54576357558a4edfdb6e97
martin.frank <martin.frank.private@gmail.com>
2019-07-24 07:55:32 +0200

    https://github.com/martinFrank/idlelib/projects/1#card-24336551

---
+++ src/main/java/com/github/martinfrank/idlelib/Timer.java
@@ -0,0 +1,28 @@
+package com.github.martinfrank.idlelib;
+
+public class Timer {
+
+
+    private final ResourceManager resourceManager;
+
+    public Timer (ResourceMananger resourceMananger){
+        this.resourceManager = resourceMananger;
+    }
+
+    public void start(){
+
+    }
+
+    public void stop(){
+
+    }
+
+    public void register(TimedComponent component){
+
+    }
+
+    public void deregister(TimedComponent component){
+
+    }
+
+}
