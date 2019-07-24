dcd51c0ff8e6501d98839ca8fdb0d499ad152e06
martin.frank <martin.frank.private@gmail.com>
2019-07-24 12:19:48 +0200

    https://github.com/martinFrank/idlelib/projects/1#card-24347161

--- src/main/java/com/github/martinfrank/idlelib/Site.java
+++ src/main/java/com/github/martinfrank/idlelib/Site.java
@@ -1,6 +1,10 @@
 package com.github.martinfrank.idlelib;
 
+import com.github.martinfrank.geolib.GeoPoint;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Site<R extends Resource<? extends ResourceType> > {
@@ -8,10 +12,17 @@ public class Site<R extends Resource<? extends ResourceType> > {
     private final ResourceManager<R> resourceManager;
 
     private final Map<Location,TimedComponent<R>> components = new HashMap<>();
+    private final List<GeoPoint> sitePoints = new ArrayList<>();
 
 
     public Site(ResourceManager<R> resourceManager){
         this.resourceManager = resourceManager;
+        //FIXME: constructor with predefined site size
+        for (int dy = 0; dy < 10; dy++) {
+            for (int dx = 0; dx < 10; dx++) {
+                sitePoints.add(new GeoPoint(dx, dy));
+            }
+        }
     }
 
     public void add(TimedComponent<R> timedComponent, Location location){
@@ -20,6 +31,17 @@ public class Site<R extends Resource<? extends ResourceType> > {
     }
 
     public boolean isLocationAvailable(Location location){
+        List<GeoPoint> free = new ArrayList<>(sitePoints);
+        for (Location occupied : components.keySet()) {
+            for (GeoPoint point : occupied.getLocationPoints()) {
+                free.remove(point);
+            }
+        }
+        for (GeoPoint target : location.getLocationPoints()) {
+            if (!free.contains(target)) {
+                return false;
+            }
+        }
         return true;
     }
 
--- src/test/java/com/github/martinfrank/idlelib/TimerTest.java
+++ src/test/java/com/github/martinfrank/idlelib/TimerTest.java
@@ -1,8 +1,11 @@
 package com.github.martinfrank.idlelib;
 
+import com.github.martinfrank.geolib.GeoPoint;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class TimerTest {
@@ -13,22 +16,33 @@ public class TimerTest {
         Timer timer = new Timer();
         ResourceManager<TestResource> resourceManager = new ResourceManager<>(timer);
         Site<TestResource> site = new Site<>(resourceManager);
-        TestTimedComponent testTimedComponent = new TestTimedComponent();
-        Location location = new Location(){};
-        site.add(testTimedComponent, location);
+        TimedComponent<TestResource> timedComponent =
+                new TimedComponent<>(750, TimeUnit.MILLISECONDS, calculateYield(), new Shape());
+        Location location = new Location(new GeoPoint(0, 0), timedComponent.getShape());
 
+        Assert.assertTrue(site.isLocationAvailable(location));
+        site.add(timedComponent, location);
+        Assert.assertFalse(site.isLocationAvailable(location));
 
         Assert.assertEquals(0, resourceManager.getResource(TestResourceType.TIMBER), 0.1 );
         TimeUnit.SECONDS.sleep(1);
-        Assert.assertTrue(testTimedComponent.isComplete());
+        Assert.assertTrue(timedComponent.isComplete());
         Assert.assertEquals(0, resourceManager.getResource(TestResourceType.TIMBER), 0.1 );
 
-        testTimedComponent.yield();
+        timedComponent.yield();
 
-        Assert.assertFalse(testTimedComponent.isComplete());
+        Assert.assertFalse(timedComponent.isComplete());
         Assert.assertEquals(1, resourceManager.getResource(TestResourceType.TIMBER), 0.1 );
 
         timer.stop();
     }
 
+
+    private List<TestResource> calculateYield() {
+        List<TestResource> resources = new ArrayList<>();
+        TestResource timber = new TestResource(TestResourceType.TIMBER);
+        timber.setAmount(1);
+        resources.add(timber);
+        return resources;
+    }
 }
